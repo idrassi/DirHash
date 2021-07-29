e97546698922287a27ac14671852a8fd15fe0ac0
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2021-07-30 00:42:20 +0200

    use correct timestamp server for code signing

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 PATH=%PATH%;%WSDK81%\bin\x86
 
 rem sign using SHA-256
-signtool sign /v /sha1 04141E4EA6D9343CEC994F6C099DC09BDD8937C9 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 "Release\DirHash.exe" "x64\Release\DirHash.exe" "ARM64\Release\DirHash.exe" 
+signtool sign /v /sha1 04141E4EA6D9343CEC994F6C099DC09BDD8937C9 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 "Release\DirHash.exe" "x64\Release\DirHash.exe" "ARM64\Release\DirHash.exe" 
 
 pause
