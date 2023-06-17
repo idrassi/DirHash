1b92049a0967bd706331a1c1754567c3906695cb
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2023-06-17 22:36:07 +0200

    Update sign.bat to use new code signing certificate

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 PATH=%PATH%;%WSDK81%\bin\x86
 
 rem sign using SHA-256
-signtool sign /v /sha1 2B174F12D921AF2FF576D867BE91E97E4ADC7D07 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 "Release\DirHash.exe" "x64\Release\DirHash.exe" "ARM64\Release\DirHash.exe" 
+signtool sign /v /sha1 88c1ff4b7469ea3915bd8e7635a7567d34f43202 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.digicert.com /td SHA256 "Release\DirHash.exe" "x64\Release\DirHash.exe" "ARM64\Release\DirHash.exe" 
 
 pause
