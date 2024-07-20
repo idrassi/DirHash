b3ff3384ae8d91d8d6d15d4203d9260b1743eebc
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2024-07-21 01:39:24 +0200

    Add Windows 10 kit path to sign.bat

--- sign.bat
+++ sign.bat
@@ -1,4 +1,4 @@
-PATH=%PATH%;%WSDK81%\bin\x86
+PATH=%PATH%;%WSDK81%\bin\x86;C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x86
 
 rem sign using SHA-256
 signtool sign /v /sha1 88c1ff4b7469ea3915bd8e7635a7567d34f43202 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.digicert.com /td SHA256 "Release\DirHash.exe" "x64\Release\DirHash.exe" "ARM64\Release\DirHash.exe" 
